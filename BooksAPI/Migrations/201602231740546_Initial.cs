fea7b3473c553644bce17ae9f8f37fee977faccc
magqatest <nicholasi@magenic.com>
2016-03-16 09:48:15 -0500

    Adding libraries and source documents

---
+++ BooksAPI/Migrations/201602231740546_Initial.cs
@@ -0,0 +1,45 @@
+namespace BooksAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        BookId = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Genre = c.String(),
+                        PublishDate = c.DateTime(nullable: false),
+                        Description = c.String(),
+                        AuthorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.BookId)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        AuthorId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.AuthorId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropTable("dbo.Authors");
+            DropTable("dbo.Books");
+        }
+    }
+}
---
+++ BooksAPI/Models/BooksAPIContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+
+namespace BooksAPI.Models
+{
+    public class BooksAPIContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<BooksAPI.Models.BooksAPIContext>());
+
+        public BooksAPIContext() : base("name=BooksAPIContext")
+        {
+        }
+
+        public DbSet<Book> Books
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Author> Authors
+        {
+            get;
+            set;
+        }
+    }
+}
